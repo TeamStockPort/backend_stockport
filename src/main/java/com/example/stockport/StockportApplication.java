09a93ad2498ed28405431721c9fa7d76184c78b9
박태진 <ptj7149@naver.com>
2025-05-19 12:42:28 +0900

    first commit

---
+++ src/main/java/com/example/stockport/StockportApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stockport/StockportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
